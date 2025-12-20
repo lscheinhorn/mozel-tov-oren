013c6c8deb5bafe1e5243d05a9dfad46bad0ede9
lscheinhorn <lukescheinhorn@gmail.com>
2025-12-20 11:38:11 -0500

    fix: hasrouter for pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Red from './Red/Red';
 import Bull from './Bull/Bull';
